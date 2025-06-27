a54603717249c52ff9fdb4c1733041a172aab048
AdDoha0 <adamdoxaev@gmail.com>
2025-06-27 18:13:10 +0300

    prepared the structure for the backend

---
+++ backend/target/debug/deps/backend-94acdb3b71d68795.d
@@ -0,0 +1,5 @@
+/home/user/my_projects/huna_arabic/backend/target/debug/deps/libbackend-94acdb3b71d68795.rmeta: src/main.rs
+
+/home/user/my_projects/huna_arabic/backend/target/debug/deps/backend-94acdb3b71d68795.d: src/main.rs
+
+src/main.rs:
